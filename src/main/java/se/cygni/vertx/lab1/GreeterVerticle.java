019316f955a9f438235a2de2a77c87846733a3aa
Henrik Lundgren <henrik.lundgren@springworks.se>
2016-04-17 19:52:29 +0200

    Solution for lab1

--- src/main/java/se/cygni/vertx/lab1/GreeterVerticle.java
+++ src/main/java/se/cygni/vertx/lab1/GreeterVerticle.java
@@ -7,7 +7,16 @@ public class GreeterVerticle extends AbstractVerticle {
   @Override
   public void start() throws Exception {
     vertx.eventBus().<String>consumer("greet.echo", message -> {
-
+      message.reply("Hello " + message.body());
     });
+
+    vertx.eventBus().<String>consumer("greet.shared-data", message -> {
+      String greetformat = greetFormat();
+      message.reply(String.format(greetformat, message.body()));
+    });
+  }
+
+  private String greetFormat() {
+    return vertx.sharedData().<String, String >getLocalMap("shared-data").get("greetformat");
   }
 }
